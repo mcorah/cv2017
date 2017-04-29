2743c23e5372126487ae97db8e65a1abe724e50f
Micah Corah <micahcorah@gmail.com>
2017-04-29 14:53:49 -0400

    script to create miniture nyu datasets

---
+++ mini_nyu.m
@@ -0,0 +1,18 @@
+clear all; close all; clc
+
+disp('shrinking nyusplpits.mat to create nuyusplits_mini.mat')
+
+reduction_factor = 40;
+
+load('../data/benchmarkData/metadata/nyusplits.mat')
+val = val(1:floor(end/reduction_factor));
+train = train(1:floor(end/reduction_factor));
+test = test(1:floor(end/reduction_factor));
+train1 = train1(1:floor(end/reduction_factor));
+train2 = train2(1:floor(end/reduction_factor));
+val1 = val1(1:floor(end/reduction_factor));
+val2 = val2(1:floor(end/reduction_factor));
+trainval = trainval(1:floor(end/reduction_factor));
+save('../data/benchmarkData/metadata/nyusplits_mini.mat')
+
+disp('done')
