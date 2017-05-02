46ad90cf8ac198cbf01141824d009706c71adb31
Micah Corah <micahcorah@gmail.com>
2017-05-02 13:32:11 -0400

    merging feature/maitreya

--- segmentation/computeUCMFeatures.m
+++ segmentation/computeUCMFeatures.m
@@ -22,10 +22,14 @@ function computeUCMFeatures(imSet, paths, forTraining)
 		end
 	end
 
-	parfor i = 1:length(imList),
+	for i = 1:length(imList),
 		tt = tic();
 		sPb2 = []; thr = [];
 		imName = imList{i};
+        dName = fullfile(paths.ucmFDir);
+		if(~exist(dName, 'dir'))
+			mkdir(dName);
+		end
 		fileName = fullfile(paths.ucmFDir, sprintf('%s.mat', imName));
 		try
 			dt1 = load(fileName); 
@@ -39,9 +43,11 @@ function computeUCMFeatures(imSet, paths, forTraining)
 			pcf = getFilledPointCloud(imName);
 
 			try
+                %Have to replace computeDepthCues in computeLocalCues
 				[bg, cga, cgb, tg, ng1, ng2, dg, z, cues] = computeLocalCues(imName, paths, I, pc, pcf, param);
 				[img_features, img_ids, sPb2, thr, ucm2] = compute_image_features(cues, dt); %This is not the final UCM yet.
 				parsave(fileName, 'img_features', img_features, 'thr', thr, 'img_ids', img_ids, 'sPb2', sPb2, 'ucm2', ucm2);
+                fprintf('Completed Local Cues for image %s.\n', imName);
 			catch ee
 				prettyexception(ee);
 				fprintf('Something went wrong while computing UCM cues for image %s.\n', imName);
@@ -50,6 +56,10 @@ function computeUCMFeatures(imSet, paths, forTraining)
 	
 		if(forTraining)
 			% Compute the ground truth to use for the boundary candidates..
+            dName = fullfile(paths.ucmGTDir);
+            if(~exist(dName, 'dir'))
+                mkdir(dName);
+            end
 			gtOutFile = fullfile(paths.ucmGTDir, sprintf('%s.mat', imName))
 			try
 				dt2 = load(gtOutFile);
@@ -61,6 +71,7 @@ function computeUCMFeatures(imSet, paths, forTraining)
 					ucm_gto = transferGroundTruth(groundTruth, sPb2, thr);
 					parsave(gtOutFile, 'ucm_gto', ucm_gto);
 				catch e
+                    prettyexception(e);
 					fprintf('Something went wrong while computing ground truth for image %s.\n', imName);
 				end
 			end
